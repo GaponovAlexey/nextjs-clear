8d6c4a11f326afca66bae3175618ef53ac9fdb3c
kekelberi <alexei.gaponov@gmail.com>
2021-11-22 16:24:41 +0300

    save

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ export default function Home() {
   background: ${props => props.primary ? "palevioletred" : "white"};
   color: ${props => props.primary ? "white" : "palevioletred"};
 
+  
   font-size: 1em;
   margin: 1em;
   padding: 0.25em 1em;
